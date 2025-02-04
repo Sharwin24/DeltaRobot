a81f0f67bcca3d1b7c564356ead84f5d0dad934e
Sharwin24 <sharwin24@gmail.com>
2025-02-04 16:52:54 -0600

    Wrote motor control node with subscriber

---
+++ deltarobot_motor_control/include/deltarobot_motor_control/delta_motor_control.hpp
@@ -0,0 +1,31 @@
+#ifndef DELTA_MOTOR_CONTROL_HPP_
+#define DELTA_MOTOR_CONTROL_HPP_
+
+#include <cstdio>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "rcutils/cmdline_parser.h"
+#include "dynamixel_sdk/dynamixel_sdk.h"
+#include "deltarobot_interfaces/msg/position_cmd.hpp"
+
+
+class DeltaMotorControl : public rclcpp::Node {
+public:
+  using PositionCmd = deltarobot_interfaces::msg::PositionCmd;
+  using GetPositions = deltarobot_interfaces::srv::GetPositions;
+
+  DeltaMotorControl();
+  ~DeltaMotorControl() = default;  
+
+private:
+  rclcpp::Subscription<PositionCmd>::SharedPtr position_cmd_subscriber;
+  rclcpp::Service<GetPositions>::SharedPtr get_positions_server;
+
+  dynamixel::PortHandler* portHandler;
+  dynamixel::PacketHandler* packetHandler;
+
+};
+
+#endif  // DELTA_MOTOR_CONTROL_HPP_
---
+++ deltarobot_motor_control/src/delta_motor_control.cpp
@@ -0,0 +1,207 @@
+#include "delta_motor_control.hpp"
+
+// Control table address for X series(except XL - 320)
+#define ADDR_OPERATING_MODE 11
+#define ADDR_TORQUE_ENABLE 64
+#define ADDR_GOAL_POSITION 116
+#define ADDR_PRESENT_POSITION 132
+
+// Protocol version
+#define PROTOCOL_VERSION 2.0  // Default Protocol version of DYNAMIXEL X series.
+
+// Default setting
+#define BAUDRATE 57600  // Default Baudrate of DYNAMIXEL X series
+#define DEVICE_NAME "/dev/ttyUSB0"  // [Linux]: "/dev/ttyUSB*", [Windows]: "COM*"
+
+DeltaMotorControl::DeltaMotorControl() : Node("delta_motor_control") {
+  RCLCPP_INFO(this->get_logger(), "DeltaMotorControl Started");
+
+  this->declare_parameter("qos_depth", 10);
+  int8_t qos_depth = 0;
+  this->get_parameter("qos_depth", qos_depth);
+
+  const auto QOS_RKL10V =
+    rclcpp::QoS(rclcpp::KeepLast(qos_depth)).reliable().durability_volatile();
+
+  this->portHandler = dynamixel::PortHandler::getPortHandler(DEVICE_NAME);
+  this->packetHandler = dynamixel::PacketHandler::getPacketHandler(PROTOCOL_VERSION);
+
+  int dxl_comm_result = COMM_TX_FAIL;
+  uint8_t dxl_error = 0;
+
+  // Open Serial Port
+  dxl_comm_result = this->portHandler->openPort();
+  if (dxl_comm_result == false) {
+    RCLCPP_ERROR(rclcpp::get_logger("read_write_node"), "Failed to open the port!");
+    return -1;
+  }
+  else {
+    RCLCPP_INFO(rclcpp::get_logger("read_write_node"), "Succeeded to open the port.");
+  }
+
+  // Set the baudrate of the serial port (use DYNAMIXEL Baudrate)
+  dxl_comm_result = this->portHandler->setBaudRate(BAUDRATE);
+  if (dxl_comm_result == false) {
+    RCLCPP_ERROR(rclcpp::get_logger("read_write_node"), "Failed to set the baudrate!");
+    return -1;
+  }
+  else {
+    RCLCPP_INFO(rclcpp::get_logger("read_write_node"), "Succeeded to set the baudrate.");
+  }
+
+  // Use Position Control Mode
+  dxl_comm_result = this->packetHandler->write1ByteTxRx(
+    this->portHandler,
+    BROADCAST_ID,
+    ADDR_OPERATING_MODE,
+    3,
+    &dxl_error
+  );
+
+  if (dxl_comm_result != COMM_SUCCESS) {
+    RCLCPP_ERROR(rclcpp::get_logger("read_write_node"), "Failed to set Position Control Mode.");
+  }
+  else {
+    RCLCPP_INFO(rclcpp::get_logger("read_write_node"), "Succeeded to set Position Control Mode.");
+  }
+
+  // Enable Torque of DYNAMIXEL
+  dxl_comm_result = packetHandler->write1ByteTxRx(
+    portHandler,
+    BROADCAST_ID,
+    ADDR_TORQUE_ENABLE,
+    1,
+    &dxl_error
+  );
+
+  if (dxl_comm_result != COMM_SUCCESS) {
+    RCLCPP_ERROR(rclcpp::get_logger("read_write_node"), "Failed to enable torque.");
+  }
+  else {
+    RCLCPP_INFO(rclcpp::get_logger("read_write_node"), "Succeeded to enable torque.");
+  }
+
+  // Subscriber to receive position commands and write them to the motors
+  position_cmd_subscriber =
+    this->create_subscription<PositionCmd>(
+      "set_motor_positions",
+      QOS_RKL10V,
+      [this](const PositionCmd::SharedPtr msg) -> void
+      {
+        uint8_t dxl_error = 0;
+        int dxl_comm_result = COMM_TX_FAIL;
+
+        // Position Value of X series is 4 byte data.
+        // For AX & MX(1.0) use 2 byte data(uint16_t) for the Position Value.
+        uint32_t motor1_position = (unsigned int)msg->motor1_pos;  // Convert int32 -> uint32
+        uint32_t motor2_position = (unsigned int)msg->motor2_pos;  // Convert int32 -> uint32
+        uint32_t motor3_position = (unsigned int)msg->motor3_pos;  // Convert int32 -> uint32
+
+        // Write Motor 1 position (length : 4 bytes)
+        dxl_comm_result =
+          this->packetHandler->write4ByteTxRx(
+            this->portHandler,
+            1,
+            ADDR_GOAL_POSITION,
+            motor1_position,
+            &dxl_error
+          );
+
+        if (dxl_comm_result != COMM_SUCCESS) {
+          RCLCPP_INFO(this->get_logger(), "%s", this->packetHandler->getTxRxResult(dxl_comm_result));
+        }
+        else if (dxl_error != 0) {
+          RCLCPP_INFO(this->get_logger(), "%s", this->packetHandler->getRxPacketError(dxl_error));
+        }
+
+        // Write Motor 2 position (length : 4 bytes)
+        dxl_comm_result =
+          this->packetHandler->write4ByteTxRx(
+            this->portHandler,
+            2,
+            ADDR_GOAL_POSITION,
+            motor2_position,
+            &dxl_error
+          );
+
+        if (dxl_comm_result != COMM_SUCCESS) {
+          RCLCPP_INFO(this->get_logger(), "%s", this->packetHandler->getTxRxResult(dxl_comm_result));
+        }
+        else if (dxl_error != 0) {
+          RCLCPP_INFO(this->get_logger(), "%s", this->packetHandler->getRxPacketError(dxl_error));
+        }
+
+        // Write Motor 3 position (length : 4 bytes)
+        dxl_comm_result =
+          this->packetHandler->write4ByteTxRx(
+            this->portHandler,
+            3,
+            ADDR_GOAL_POSITION,
+            motor3_position,
+            &dxl_error
+          );
+
+        if (dxl_comm_result != COMM_SUCCESS) {
+          RCLCPP_INFO(this->get_logger(), "%s", this->packetHandler->getTxRxResult(dxl_comm_result));
+        }
+        else if (dxl_error != 0) {
+          RCLCPP_INFO(this->get_logger(), "%s", this->packetHandler->getRxPacketError(dxl_error));
+        }
+      }
+    );
+  
+  // Service to get the current motor positions
+  auto get_positions =
+    [this](
+      const std::shared_ptr<GetPositions::Request> request,
+      std::shared_ptr<GetPositions::Response> response) -> void
+    {
+      uint8_t dxl_error = 0;
+      int dxl_comm_result = COMM_TX_FAIL;
+
+      int motor1 = 0;
+      int motor2 = 0;
+      int motor3 = 0;
+
+      // Read Present Position (length : 4 bytes) and Convert uint32 -> int32
+      // When reading 2 byte data from AX / MX(1.0), use read2ByteTxRx() instead.
+      dxl_comm_result = this->packetHandler->read4ByteTxRx(
+        this->portHandler,
+        1,
+        ADDR_PRESENT_POSITION,
+        reinterpret_cast<uint32_t*>(&motor1),
+        &dxl_error
+      );
+
+      dxl_comm_result = this->packetHandler->read4ByteTxRx(
+        this->portHandler,
+        2,
+        ADDR_PRESENT_POSITION,
+        reinterpret_cast<uint32_t*>(&motor2),
+        &dxl_error
+      );
+
+      dxl_comm_result = this->packetHandler->read4ByteTxRx(
+        this->portHandler,
+        3,
+        ADDR_PRESENT_POSITION,
+        reinterpret_cast<uint32_t*>(&motor3),
+        &dxl_error
+      );
+
+      RCLCPP_INFO(
+        this->get_logger(),
+        "Motor Positions: Motor1: %d, Motor2: %d, Motor3: %d",
+        motor1,
+        motor2,
+        motor3
+      );
+
+      response->motor1_position = motor1;
+      response->motor2_position = motor2;
+      response->motor3_position = motor3;
+    };
+
+  get_positions_server = create_service<GetPositions>("get_motor_positions", get_positions);
+
+}
