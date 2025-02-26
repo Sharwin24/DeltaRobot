1c9b9e5bf882e71e62d408cff6f189b851861cf5
Sharwin Patil <sharwin24@gmail.com>
2025-02-26 13:03:00 -0600

    pringle trajectory

--- deltarobot_control/src/delta_test.cpp
+++ deltarobot_control/src/delta_test.cpp
@@ -10,7 +10,8 @@
 #include "deltarobot_interfaces/srv/play_fk_trajectory.hpp"
 #include "deltarobot_interfaces/srv/play_ik_trajectory.hpp"
 #include "geometry_msgs/msg/point.hpp"
-#include "std_msgs/msg/string.hpp"
+
+#include <math.h>
 
 using Point = geometry_msgs::msg::Point;
 
@@ -42,6 +43,8 @@ void DeltaTest::testTrajectory(
   std::string type = request->type.data;
   if (type == "straight_up_down") {
     response->success = this->straightUpDownTrajectory();
+  } else if (type == "pringle") {
+    response->success = this->pringleTrajectory();
   } else {
     RCLCPP_ERROR(get_logger(), "Invalid trajectory type: %s", type.c_str());
     response->success = false;
@@ -131,6 +134,84 @@ bool DeltaTest::straightUpDownTrajectory() {
   return true;
 }
 bool DeltaTest::pringleTrajectory() {
+  // Circle Trajectory in XY plane while Z coordinate goes through 2 cycles of a sine wave
+  const int num_points = 100;
+  const float circle_center_z = -180.0;
+  const float amplitude = 25.0;
+
+  std::vector<float> t(num_points);
+  float step = (2 * M_PI) / (num_points - 1);
+  for (int i = 0; i < num_points; ++i) {
+    t[i] = i * step;
+  }
+
+  std::vector<float> x_circle(num_points);
+  std::vector<float> y_circle(num_points);
+  std::vector<float> z_circle(num_points);
+  for (int i = 0; i < num_points; ++i) {
+    x_circle[i] = (2.0 * amplitude) * cos(t[i]);
+    y_circle[i] = (2.0 * amplitude) * sin(t[i]);
+    z_circle[i] = circle_center_z + amplitude * sin(2 * t[i]);
+  }
+
+  // Create trajectory
+  std::vector<Point> trajectory(num_points);
+  for (int i = 0; i < num_points; ++i) {
+    trajectory[i].x = x_circle[i];
+    trajectory[i].y = y_circle[i];
+    trajectory[i].z = z_circle[i];
+  }
+
+  // Log the created trajectory
+  RCLCPP_INFO(get_logger(), "EE Trajectory created with %ld points:", trajectory.size());
+  for (int i = 0; i < num_points; i++) {
+    Point p = trajectory[i];
+    RCLCPP_INFO(get_logger(), "\t EE Point %d: (%.2f, %.2f, %.2f)", i + 1, p.x, p.y, p.z);
+  }
+
+  // Convert the end-effector trajectory into a joint trajectory using the IK service
+  auto joint_trajectory = std::make_shared<std::vector<deltarobot_interfaces::msg::DeltaJoints>>();
+
+  for (int i = 0; i < num_points; i++) {
+    // Create IK request
+    auto ik_request = std::make_shared<deltarobot_interfaces::srv::DeltaIK::Request>();
+    ik_request->solution.x = trajectory[i].x;
+    ik_request->solution.y = trajectory[i].y;
+    ik_request->solution.z = trajectory[i].z;
+
+    this->delta_ik_client->async_send_request(
+      ik_request,
+      [this, i, joint_trajectory, num_points](rclcpp::Client<deltarobot_interfaces::srv::DeltaIK>::SharedFuture future) {
+        auto ik_response = future.get();
+        joint_trajectory->push_back(ik_response->joint_angles);
+
+        if (joint_trajectory->size() == static_cast<size_t>(num_points)) {
+          // Log the joint trajectory
+          RCLCPP_INFO(this->get_logger(), "Joint trajectory created with %ld points:", joint_trajectory->size());
+          for (int j = 0; j < num_points; j++) {
+            const auto& joints = joint_trajectory->at(j);
+            RCLCPP_INFO(this->get_logger(), "\t Joint Angles %d: (%.2f, %.2f, %.2f) [rad]", j, joints.theta1, joints.theta2, joints.theta3);
+          }
+
+          // Publish the joint trajectory messages to the topic "/set_joints"
+          for (int i = 0; i < num_points; i++) {
+            // Create a message with the joint angles
+            auto joint_msg = deltarobot_interfaces::msg::DeltaJoints();
+            joint_msg.theta1 = joint_trajectory->at(i).theta1;
+            joint_msg.theta2 = joint_trajectory->at(i).theta2;
+            joint_msg.theta3 = joint_trajectory->at(i).theta3;
+
+            // Publish the message
+            this->joint_pub->publish(joint_msg);
+            // Let the robot read and move
+            rclcpp::sleep_for(std::chrono::milliseconds(50));
+          }
+        }
+      }
+    );
+  }
+
+  // Signal success after service is finished
   return true;
 }
 
