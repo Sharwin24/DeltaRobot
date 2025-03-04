d59b2511811e3a1fe3944ae5b6b7fba1f5178e73
Sharwin24 <sharwin24@gmail.com>
2025-03-03 19:50:02 -0600

    Convert to joint vel client

--- deltarobot_trajectory_generator/include/deltarobot_trajectory_generator/delta_trajectory_generator.hpp
+++ deltarobot_trajectory_generator/include/deltarobot_trajectory_generator/delta_trajectory_generator.hpp
@@ -5,10 +5,13 @@
 #include "deltarobot_interfaces/msg/delta_joints.hpp"
 #include "deltarobot_interfaces/srv/delta_ik.hpp"
 #include "deltarobot_interfaces/srv/convert_to_joint_trajectory.hpp"
+#include "deltarobot_interfaces/srv/convert_to_joint_vel_trajectory.hpp"
 #include "geometry_msgs/msg/point.hpp"
 
 using DeltaIK = deltarobot_interfaces::srv::DeltaIK;
 using ConvertToJointTrajectory = deltarobot_interfaces::srv::ConvertToJointTrajectory;
+using ConvertToJointVelTrajectory = deltarobot_interfaces::srv::ConvertToJointVelTrajectory;
+
 using Point = geometry_msgs::msg::Point;
 
 class DeltaTrajectoryGenerator : public rclcpp::Node {
@@ -20,6 +23,8 @@ class DeltaTrajectoryGenerator : public rclcpp::Node {
   // Create a IK client to call the DeltaIK Service
   rclcpp::Client<DeltaIK>::SharedPtr delta_ik_client;
   rclcpp::Client<ConvertToJointTrajectory>::SharedPtr convert_to_joint_trajectory_client;
+  rclcpp::Client<ConvertToJointVelTrajectory>::SharedPtr convert_to_joint_vel_trajectory_client;
+
 };
 
 #endif // !DELTA_TRAJ_GEN_HPP_
